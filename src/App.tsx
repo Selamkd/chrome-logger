18a09eb150e76f9bfedb224ad0fd662e3a8df76a
“selamkd” <selamkedir@icloud.com>
2026-01-27 11:14:19 +0000

    install React + TS, tailwind and add manifest file

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      
+      </div>
+      <h1>Chrome Logger</h1>
+     
+      
+    </>
+  )
+}
+
+export default App
